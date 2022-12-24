d929cc0d76652be6ff6810a7229b81ae6ec92bc3
RickySin11 <69612398+RickySin11@users.noreply.github.com>
2022-12-23 16:30:08 -0800

    Outlined Class Structure of Habit Logger Connection Class

---
+++ HabitLoggerLibrary/HabitLogger.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitLoggerLibrary;
+
+
+// A wrapper class for the SqliteConnection class which connects
+// to the Habit Logger data base.
+//
+// This class is only fitted for the HabitLogger Application. This will
+// cause errors if two instances of this clas are made within the same scope.
+// This class is not suited for multi-threaded uses.
+public class HabitLoggerConnection : IDisposable
+{
+    private static string DatabaseFilePath { get; } = @"Data Source=habitlogger.db";
+
+    private SqliteConnection Connection { get; }
+
+
+    public HabitLoggerConnection()
+    {
+        Connection = new(DatabaseFilePath);
+        Connection.Open();
+        EnsureTablesExist();
+    }
+
+    private void EnsureTablesExist()
+    {
+        string createTable = @"CREATE TABLE IF NOT EXISTS DRINK_WATER {
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Date DATETIME,
+                            Quantity INTEGER
+                            }";
+
+        SqliteCommand command = new(createTable, Connection);
+        command.ExecuteNonQuery();
+    }
+
+    public void CreateLog()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public string DisplayLogs()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteLog()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateLog()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        Connection.Dispose();
+    }
+}
+
